71fa658246aa0739d103cfab85875d282b550b21
GymnastiarAnwar <140226210+GymnastiarAnwar@users.noreply.github.com>
2024-05-13 10:46:09 +0700

    Update NormalZomB.java

--- src/entity/Zombies/NormalZomB.java
+++ src/entity/Zombies/NormalZomB.java
@@ -6,6 +6,10 @@ public class NormalZomB extends Zombie {
         super("Normal Zombie", 125, 100, 1, false);
     }
 
+    public NormalZomB(int extra_health) {
+        super(125 + extra_health, 100, 1, false);
+    }
+
     public  void attack() {
         
     }
